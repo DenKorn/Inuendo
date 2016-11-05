2cacee1e2d44298267da194cca0349624d4a7742
Денис <kornejchukdenis@gmail.com>
2016-11-05 13:59:40 +0200

    Корректировка внешних ключей
    Внешние ключи или не создавались, или создавались неверно из-за ряда
    помарок. Исправлено.

--- migrations/m161008_114213_db_init.php
+++ migrations/m161008_114213_db_init.php
@@ -6,14 +6,18 @@ class m161008_114213_db_init extends Migration
 {
     public function up()
     {
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE=InnoDB';
+        }
 
         $this->createTable('users', [
-            'id' => 'INT NOT NULL',
+            'id' => 'INT UNSIGNED NOT NULL',
             'login' => 'VARCHAR(45) NOT NULL',
             'password' => 'VARCHAR(45) NULL',
             'last_visit' => 'DATETIME NULL DEFAULT NOW()',
             'PRIMARY KEY (`id`)'
-        ]);
+        ],$tableOptions);
 
         $this->createTable('advertisements', [
             'id' => 'INT UNSIGNED NOT NULL AUTO_INCREMENT',
@@ -27,7 +31,7 @@ class m161008_114213_db_init extends Migration
             'target_reference' => 'VARCHAR(400) NULL',
             'configuration' => 'MEDIUMTEXT NULL',
             'PRIMARY KEY (`id`)'
-        ]);
+        ],$tableOptions);
 
         $this->createTable('ignore_list', [
             'id' => 'INT UNSIGNED NOT NULL AUTO_INCREMENT',
@@ -35,7 +39,7 @@ class m161008_114213_db_init extends Migration
             'block_period' => 'INT UNSIGNED NULL DEFAULT 7 COMMENT \'Block period in days. \"-1\" mean infinite users ignoring.\'',
             'reason' => 'VARCHAR(200) NULL',
             'PRIMARY KEY (`id`)'
-        ]);
+        ],$tableOptions);
 
         $this->createTable('ban_list', [
             'id' => 'INT UNSIGNED NOT NULL AUTO_INCREMENT',
@@ -43,7 +47,7 @@ class m161008_114213_db_init extends Migration
             'ban_period' => 'INT UNSIGNED NULL DEFAULT 7',
             'reason' => 'VARCHAR(200) NULL',
             'PRIMARY KEY (`id`)'
-        ]);
+        ],$tableOptions);
 
         $this->createTable('advert_moder_turn', [
             'id' => 'INT UNSIGNED NOT NULL AUTO_INCREMENT',
@@ -52,7 +56,7 @@ class m161008_114213_db_init extends Migration
             'created' => 'DATETIME NOT NULL DEFAULT NOW()',
             'comment' => 'VARCHAR(300) NULL',
             'PRIMARY KEY (`id`)'
-        ]);
+        ],$tableOptions);
 
         $this->createTable('internet_platforms', [
             'id' => 'INT UNSIGNED NOT NULL AUTO_INCREMENT',
@@ -60,7 +64,7 @@ class m161008_114213_db_init extends Migration
             'siteURL' => 'MEDIUMTEXT BINARY NULL',
             'configuration' => 'LONGTEXT NULL',
             'PRIMARY KEY (`id`)'
-        ]);
+        ],$tableOptions);
 
         $this->createTable('paying_plans', [
             'id' => 'INT UNSIGNED NOT NULL AUTO_INCREMENT',
@@ -69,7 +73,7 @@ class m161008_114213_db_init extends Migration
             'expiration_time' => 'DATETIME NOT NULL',
             'cost' => 'INT UNSIGNED NOT NULL DEFAULT 0',
             'PRIMARY KEY (`id`)'
-        ]);
+        ],$tableOptions);
 
         $this->createTable('support_dialogs', [
             'id' => 'INT UNSIGNED NOT NULL AUTO_INCREMENT',
@@ -78,7 +82,7 @@ class m161008_114213_db_init extends Migration
             'send_time' => 'DATETIME NULL DEFAULT NOW()',
             'message' => 'MEDIUMTEXT NOT NULL',
             'PRIMARY KEY (`id`)'
-        ]);
+        ],$tableOptions);
 
         $this->createTable('advert_rents', [
             'id' => 'INT UNSIGNED NOT NULL AUTO_INCREMENT',
@@ -88,27 +92,27 @@ class m161008_114213_db_init extends Migration
             'sum' => 'INT NOT NULL DEFAULT 0',
             'configuration' => 'LONGTEXT NULL',
             'PRIMARY KEY (`id`)'
-        ]);
+        ],$tableOptions);
 
         $this->createTable('source_message', [
             'id' => 'INT(11) AUTO_INCREMENT',
             'category' => 'VARCHAR(32)',
             'message' => 'TEXT',
             'PRIMARY KEY (id)',
-        ]);
+        ],$tableOptions);
 
         $this->createTable('message', [
             'id' => 'INT(11)',
             'language' => 'VARCHAR(16)',
             'translation' => 'TEXT',
             'PRIMARY KEY (id,language)',
-        ]);
+        ],$tableOptions);
 
         $this->createTable('session', [
             'id' => 'CHAR(40) NOT NULL PRIMARY KEY',
             'expire' => 'INTEGER',
             'data' => 'BLOB'
-        ]);
+        ],$tableOptions);
 
         //Foreign keys
         $this->addForeignKey('fk_message_source_message', 'message', 'id', 'source_message', 'id','CASCADE','RESTRICT');
@@ -118,7 +122,7 @@ class m161008_114213_db_init extends Migration
         $this->addForeignKey('fk_support_dialogs_users_recepient', 'support_dialogs', 'recepient_id', 'users', 'id','CASCADE','RESTRICT');
         $this->addForeignKey('fk_advertisements_users', 'advertisements', 'owner_id', 'users', 'id','CASCADE','RESTRICT');
         $this->addForeignKey('fk_advert_moder_turn_advertisements', 'advert_moder_turn', 'advert_id', 'advertisements', 'id','CASCADE','RESTRICT');
-        $this->addForeignKey('fk_advert_rents_advertisements', 'advert_rents', 'advert_id', 'advertisem', 'id','CASCADE','RESTRICT');
+        $this->addForeignKey('fk_advert_rents_advertisements', 'advert_rents', 'advert_id', 'advertisements', 'id','CASCADE','RESTRICT');
         $this->addForeignKey('fk_advert_rents_internet_platforms', 'advert_rents', 'platform_id', 'internet_platforms', 'id','CASCADE','RESTRICT');
         $this->addForeignKey('fk_advert_rents_paying_plans', 'advert_rents', 'pay_plan_id', 'paying_plans', 'id','CASCADE','RESTRICT');
 
@@ -127,19 +131,18 @@ class m161008_114213_db_init extends Migration
 
     public function down()
     {
-        $this->dropTable('source_message');
         $this->dropTable('message');
+        $this->dropTable('source_message');
         $this->dropTable('session');
-        $this->dropTable('users');
-        $this->dropTable('advertisements');
         $this->dropTable('ignore_list');
         $this->dropTable('ban_list');
         $this->dropTable('advert_moder_turn');
-        $this->dropTable('internet_platforms');
-        $this->dropTable('paying_plans');
         $this->dropTable('support_dialogs');
         $this->dropTable('advert_rents');
-
+        $this->dropTable('paying_plans');
+        $this->dropTable('internet_platforms');
+        $this->dropTable('advertisements');
+        $this->dropTable('users');
         return true;
     }
     
